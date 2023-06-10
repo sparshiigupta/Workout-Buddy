84f27c4cda57db80feb2b47d9e34fd0f19754f52
sparshiigupta <sparshigupta28@gmail.com>
2023-06-10 21:29:37 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
