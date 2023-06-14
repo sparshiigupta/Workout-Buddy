b7e4c3d6e1d85d1e50c7109767735db036afbdc9
sparshiigupta <sparshigupta28@gmail.com>
2023-06-14 23:56:40 +0530

    signup and login pages added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = () => {
+    logout();
+  };
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout Buddy </h1>
         </Link>
+        <nav>
+          <button onClick={handleClick}>Log out</button>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext();
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts/");
+      const json = await response.json();
+
+      if (response.ok) {
+        //response is array of objects
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
+
+    fetchWorkouts();
+  }, [dispatch]);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
     </div>
   );
 };
